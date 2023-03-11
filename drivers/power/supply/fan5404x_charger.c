908134f9a664aeaa7d05cff17ff31960d9965114
Bogdan Ionescu <bogdan.ionescu.work@gmail.com>
2023-03-11 11:22:56 +0000

    wip charger

--- drivers/power/supply/fan5404x_charger.c
+++ drivers/power/supply/fan5404x_charger.c
@@ -28,6 +28,7 @@ enum fan5404x_regs {
 	FAN5404X_REG_NTC = 0x12,
 	FAN5404X_REG_WD_CTRL = 0x13,
 	FAN5404X_REG_RESTART = 0xfa,
+	FAN5404X_REG_MAX = FAN5404X_REG_RESTART,
 };
 
 /* Register filed definitions */
@@ -81,7 +82,7 @@ enum fan5404x_safety {
 };
 
 enum fan5404x_post_chrg {
-	POST_CHRG_FIELD_PC_IT = GENMASK(2 : 0),
+	POST_CHRG_FIELD_PC_IT = GENMASK(2, 0),
 	POST_CHRG_FIELD_PC_EN = BIT(3),
 	POST_CHRG_FIELD_VBUS_LOAD = GENMASK(5, 4),
 	POST_CHRG_FIELD_BDET = GENMASK(7, 6),
@@ -157,65 +158,41 @@ enum fan5404x_vbus_load_value {
 
 enum fan5404x_restart_value {
 	RESTART = 0xb5,
-}
-
-static int max17040_prop_writeable(struct power_supply *psy,
-            enum power_supply_property psp)
-{
-   switch (psp) {
-      default:
-         return 0;
-   }
-}
-
-static int max17040_get_property(struct power_supply *psy,
-				 enum power_supply_property psp,
-				 union power_supply_propval *val)
-{
-   switch (psp) {
-      default:
-         return -EINVAL;
-   }
-}
-
-static int max17040_set_property(struct power_supply *psy,
-				 enum power_supply_property psp,
-				 const union power_supply_propval *val)
-{
-   switch (psp) {
-      default:
-         return -EINVAL;
-   }
-}
-
-static enum power_supply_property fan5404x_psy_props[] = {
-	POWER_SUPPLY_PROP_STATUS,
-   POWER_SUPPLY_PROP_AUTHENTIC,
-   POWER_SUPPLY_PROP_CHARGE_TYPE,
-	POWER_SUPPLY_PROP_HEALTH,
-   POWER_SUPPLY_PROP_VOLTAGE_MAX,
-   POWER_SUPPLY_PROP_VOLTAGE_MIN,
-   POWER_SUPPLY_PROP_VOLTAGE_MAX_DESIGN,
-   POWER_SUPPLY_PROP_VOLTAGE_MIN_DESIGN,
-   POWER_SUPPLY_PROP_CURRENT_MAX,
-   POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT,
-   POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX,
-   POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE,
-   POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE_MAX,
-   POWER_SUPPLY_PROP_INPUT_CURRENT_LIMIT,
-   POWER_SUPPLY_PROP_INPUT_VOLTAGE_LIMIT,
-   POWER_SUPPLY_PROP_CHARING_TERM_CURRENT,
-   POWER_SUPPLY_PROP_SERIAL_NUMBER,
 };
 
-static const struct power_supply_desc fan5404x_psy_desc = {
-	.name = "fan5404x",
-	.type = POWER_SUPPLY_TYPE_USB,
-	.properties = fan5404x_psy_props,
-	.num_properties = ARRAY_SIZE(fan5404x_psy_props),
-	.get_property = fan5404x_get_property,
-	.set_property = fan5404x_set_property,
-	property_is_writeable = fan5404x_property_is_writeable,
+struct fan5404x {
+	struct i2c_client *client;
+	struct regmap *regmap;
+};
+
+static enum power_supply_property fan5404x_psy_props[] = {
+	POWER_SUPPLY_PROP_STATUS, POWER_SUPPLY_PROP_CHARGE_TYPE,
+	POWER_SUPPLY_PROP_HEALTH, POWER_SUPPLY_PROP_PRESENT,
+	POWER_SUPPLY_PROP_ONLINE,
+	/*
+	POWER_SUPPLY_PROP_AUTHENTIC,
+	POWER_SUPPLY_PROP_CHARGE_TYPE,
+	POWER_SUPPLY_PROP_HEALTH,
+	POWER_SUPPLY_PROP_VOLTAGE_MAX,
+	POWER_SUPPLY_PROP_VOLTAGE_MIN,
+	POWER_SUPPLY_PROP_VOLTAGE_MAX_DESIGN,
+	POWER_SUPPLY_PROP_VOLTAGE_MIN_DESIGN,
+	POWER_SUPPLY_PROP_CURRENT_MAX,
+	POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT,
+	POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX,
+	POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE,
+	POWER_SUPPLY_PROP_CONSTANT_CHARGE_VOLTAGE_MAX,
+	POWER_SUPPLY_PROP_INPUT_CURRENT_LIMIT,
+	POWER_SUPPLY_PROP_INPUT_VOLTAGE_LIMIT,
+	POWER_SUPPLY_PROP_CHARGE_TERM_CURRENT,
+	POWER_SUPPLY_PROP_SERIAL_NUMBER,
+   */
+};
+
+static const struct regmap_config fan5404x_regmap_conf = {
+	.reg_bits = 8,
+	.val_bits = 8,
+	.max_register = FAN5404X_REG_MAX,
 };
 
 static uint8_t fan5404x_get_iocharge(const uint32_t current_ma)
@@ -280,6 +257,101 @@ static uint8_t fan5404x_get_oreg(const uint32_t voltage_mv)
 		return ((voltage_mv - 3500) / 20);
 }
 
+static int fan5404x_get_status(const struct fan5404x *const charger)
+{
+	struct device *dev = &charger->client->dev;
+	int status = POWER_SUPPLY_STATUS_UNKNOWN;
+	int err;
+	int ctrl0, ctrl1;
+
+	err = regmap_read(charger->regmap, FAN5404X_REG_CTRL0, &ctrl0);
+	err |= regmap_read(charger->regmap, FAN5404X_REG_CTRL1, &ctrl1);
+
+	if (!err) {
+		status = POWER_SUPPLY_STATUS_DISCHARGING;
+		if ((ctrl0 & CTRL0_FIELD_STAT) == STATUS_CHARGE_DONE << 4)
+			status = POWER_SUPPLY_STATUS_FULL;
+		if ((ctrl0 & CTRL0_FIELD_STAT) == STATUS_PWM_EN << 4)
+			status = (ctrl1 & CTRL1_FIELD_CE_N) ?
+					 POWER_SUPPLY_STATUS_NOT_CHARGING :
+					 POWER_SUPPLY_STATUS_CHARGING;
+	} else {
+		dev_err(dev, "failed get status");
+	}
+
+	return status;
+}
+
+static int fan5404x_prop_writeable(struct power_supply *psy,
+				   enum power_supply_property psp)
+{
+	struct fan5404x *charger = power_supply_get_drvdata(psy);
+	struct device *dev = &charger->client->dev;
+
+	dev_info(dev, "is writable: %d", psp);
+
+	switch (psp) {
+	default:
+		return 0;
+	}
+}
+
+static int fan5404x_get_property(struct power_supply *psy,
+				 enum power_supply_property psp,
+				 union power_supply_propval *val)
+{
+	struct fan5404x *charger = power_supply_get_drvdata(psy);
+	struct device *dev = &charger->client->dev;
+
+	dev_info(dev, "get_property: %d", psp);
+
+	switch (psp) {
+	case POWER_SUPPLY_PROP_STATUS:
+		val->intval = fan5404x_get_status(charger);
+		break;
+	case POWER_SUPPLY_PROP_CHARGE_TYPE:
+		val->intval = POWER_SUPPLY_CHARGE_TYPE_STANDARD;
+		break;
+	case POWER_SUPPLY_PROP_HEALTH:
+		val->intval = POWER_SUPPLY_HEALTH_GOOD;
+		break;
+	case POWER_SUPPLY_PROP_PRESENT:
+		val->intval = 1;
+		break;
+	case POWER_SUPPLY_PROP_ONLINE:
+		val->intval = 1;
+		break;
+	default:
+		return -EINVAL;
+	}
+	return 0;
+}
+
+static int fan5404x_set_property(struct power_supply *psy,
+				 enum power_supply_property psp,
+				 const union power_supply_propval *val)
+{
+	struct fan5404x *charger = power_supply_get_drvdata(psy);
+	struct device *dev = &charger->client->dev;
+
+	dev_info(dev, "set_property: %d", psp);
+
+	switch (psp) {
+	default:
+		return -EINVAL;
+	}
+}
+
+static const struct power_supply_desc fan5404x_psy_desc = {
+	.name = "fan5404x",
+	.type = POWER_SUPPLY_TYPE_USB,
+	.properties = fan5404x_psy_props,
+	.num_properties = ARRAY_SIZE(fan5404x_psy_props),
+	.get_property = fan5404x_get_property,
+	.set_property = fan5404x_set_property,
+	.property_is_writeable = fan5404x_prop_writeable,
+};
+
 static void fan5404x_task(struct work_struct *work)
 {
 	/* Every 32s we need to to reset the T32 timer to continue charging. */
@@ -288,16 +360,58 @@ static void fan5404x_task(struct work_struct *work)
 static int fan5404x_probe(struct i2c_client *client)
 {
 	int ret;
+	struct device *dev = &client->dev;
+	struct power_supply_config psy_cfg = {};
+	struct power_supply *psy;
+	struct fan5404x *charger;
+
+	charger = devm_kzalloc(dev, sizeof(*charger), GFP_KERNEL);
+	if (!charger)
+		return -ENOMEM;
+
+	i2c_set_clientdata(client, charger);
+	charger->client = client;
+
+	charger->regmap = devm_regmap_init_i2c(client, &fan5404x_regmap_conf);
+	if (IS_ERR(charger->regmap))
+		return dev_err_probe(dev, PTR_ERR(charger->regmap),
+				     "cannot allocate regmap\n");
+
+	psy_cfg.drv_data = charger;
+	psy_cfg.of_node = dev->of_node;
+   /*
+	psy_cfg.supplied_to = supplied_to;
+	psy_cfg.num_supplicants = num_supplicants;
+   */
+
+	psy = devm_power_supply_register_no_ws(dev, &fan5404x_psy_desc,
+					       &psy_cfg);
+	if (IS_ERR(psy))
+		return dev_err_probe(dev, PTR_ERR(psy), "cannot register\n");
+
+	dev_info(dev, "Driver intialised");
+
+	return 0;
 }
 
 static const struct i2c_device_id fan5404x_id[] = {
-	{ "fan5404x", 0 },
+	{ "fan54041", 0 },
+	{ "fan54042", 0 },
+	{ "fan54043", 0 },
+	{ "fan54045", 0 },
+	{ "fan54046", 0 },
+	{ "fan54047", 0 },
 	{},
 };
 MODULE_DEVICE_TABLE(i2c, fan5404x_id);
 
 static const struct of_device_id of_fan5404x_leds_match[] = {
-	{ .compatible = "on,fan5404x" },
+	{ .compatible = "onsemi,fan54040" },
+	{ .compatible = "onsemi,fan54041" },
+	{ .compatible = "onsemi,fan54042" },
+	{ .compatible = "onsemi,fan54045" },
+	{ .compatible = "onsemi,fan54046" },
+	{ .compatible = "onsemi,fan54047" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, of_fan5404x_leds_match);
